eca54599aabb0d3f89f88fe04cbb848ffbe19ec0
ISIL <18990194@mail.isil.pe>
2026-05-18 13:01:14 -0500

    Implement diabetes prediction UI with Streamlit
    Added Streamlit interface for diabetes prediction.

--- app.py
+++ app.py
@@ -3,3 +3,35 @@ import numpy as np
 import joblib
 
 modelo = joblib.load("modelo_logistic.pkl")
+
+st.title("Predicción de Diabetes")
+
+st.write("Nombre: Rosa Maria Flores Echeverria")
+st.write("Código ISIL: 18990194")
+
+pregnancies = st.number_input("Pregnancies")
+glucose = st.number_input("Glucose")
+bloodpressure = st.number_input("BloodPressure")
+skinthickness = st.number_input("SkinThickness")
+insulin = st.number_input("Insulin")
+bmi = st.number_input("BMI")
+dpf = st.number_input("DiabetesPedigreeFunction")
+age = st.number_input("Age")
+
+if st.button("Predecir"):
+
+    datos = np.array([[pregnancies,
+                       glucose,
+                       bloodpressure,
+                       skinthickness,
+                       insulin,
+                       bmi,
+                       dpf,
+                       age]])
+
+    prediccion = modelo.predict(datos)
+
+    if prediccion[0] == 1:
+        st.error("El paciente podría tener diabetes")
+    else:
+        st.success("El paciente no tendría diabetes")
